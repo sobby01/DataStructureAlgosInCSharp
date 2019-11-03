6e4d980fec1d20a40e18236e03f72739d2b564a4
Saurabh Singh <saurabh.singh@winshuttle.com>
2019-11-03 14:33:45 +0530

    Selection Sort

---
+++ DSA/DSA.Sorting/SelectionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DSA.Sorting
+{
+    public class SelectionSort: ISorting
+    {
+        public void Sort(int[] arr)
+        {
+            int size = arr.Length;
+            int min = -1;
+            int temp = 0;
+            for (int indexer = 0; indexer < size; indexer++)
+            {
+                min = indexer;
+                //Find Min and then Swap
+                for (int minIndexer = indexer + 1; minIndexer < size; minIndexer++)
+                {
+                    if (arr[min] > arr[minIndexer])
+                    {
+                        min = minIndexer;
+                    }
+                }
+
+                //Swap
+
+                temp = arr[indexer];
+                arr[indexer] = arr[min];
+                arr[min] = temp;
+            }
+        }
+    }
+}
--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DSA.Sorting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,42 @@ using System.Threading.Tasks;
 
 namespace DSA
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            int[] unSortedArray = new[] {3, 5, 1, 4, 7, 2, 8, 9, 10};
+            Console.WriteLine("unsorted Array");
+            unSortedArray.ToList().ForEach(x => Console.Write(x.ToString() + " "));
+            Console.WriteLine("\n Press number for following: \n");
+            Console.WriteLine("Press 1 for Selection Sort \n");
+            Console.WriteLine("Press 2 for Bubble Sort \n");
+            Console.WriteLine("Press 3 for Merge Sort \n");
+            Console.WriteLine("Press 4 for Quick Sort \n");
+            Console.WriteLine("Press 5 for Insertion Sort\n");
+            Console.WriteLine("Press 6 for Bucket Sort\n");
+
+            string number = Console.ReadLine();
+            int result;
+            ISorting sort = new SelectionSort();
+            if (!int.TryParse(number, out result) && (result > 0 && result <= 6))
+            {
+                Console.WriteLine("Provide the input from 1 to 6");
+            }
+            switch (number)
+            {
+                case "1":
+                    sort = new SelectionSort();
+                    sort.Sort(unSortedArray);
+                    break;
+                case "2":
+                    sort = new SelectionSort();
+                    sort.Sort(unSortedArray);
+                    break;
+            }
+
+            unSortedArray.ToList().ForEach( x=> Console.Write(x.ToString()+ " "));
+            Console.ReadKey();
         }
     }
 }
